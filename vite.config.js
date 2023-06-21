d70cc81a0e46a93125a7a3b50a45bf83bdf1c493
mani shankar <dev.mshankar@gmail.com>
2023-06-22 01:00:05 +0530

    Setup Path Resolving

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
-  }
+  },
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, './src/assets'),
+      "@Components": path.resolve(__dirname, './src/Components'),
+      "@Layouts": path.resolve(__dirname, './src/Layouts'),
+      "@Templates": path.resolve(__dirname, './src/Templates'),
+    },
+  },
 })
